993905ac3e2b361cd3caaae9b498960261a9d4a8
billey <guillaume.billey@inist.fr>
2014-01-21 15:01:08 +0100

    Testing and drawing christmas tree

--- spec/christmasSpec.js
+++ spec/christmasSpec.js
@@ -1,9 +1,37 @@
-describe("christmas Tree", function(){
+describe("The Christmas Tree", function(){
 
-	it("Devrait avoir un nombre de ligne égal au nombre demandé", function(){
+	it("Devrait avoir n+1 lignes (nombre demandé + le tronc)", function(){
 		var tree = drawTree(10);
 		var treeLines = tree.split('\n');
-		expect(treeLines.length).toBe(10);
-	})
+		expect(treeLines.length).toBe(11);
+	});
 
+	it("Devrait avoir un tronc", function(){
+		var tree = drawTree(10);
+		var treeLines = tree.split('\n');
+		var trunc = treeLines[treeLines.length-1];
+		expect(trunc).toContain("|_|");
+	});
+
+	it("La première ligne doit avoir 1 seul étoile", function(){
+		var tree = drawTree(10);
+		var treeLines = tree.split('\n');
+		var firstLine = treeLines[0];
+		expect(firstLine.split("*").length-1).toBe(1);
+	});
+
+
+	it("La 4eme ligne doit avoir 4 étoiles", function(){
+		var tree = drawTree(10);
+		var treeLines = tree.split('\n');
+		var firstLine = treeLines[3];
+		expect(firstLine.split("*").length-1).toBe(4);
+	});
+
+	it("La 5eme ligne doit avoir 3 étoiles", function(){
+		var tree = drawTree(10);
+		var treeLines = tree.split('\n');
+		var firstLine = treeLines[4];
+		expect(firstLine.split("*").length-1).toBe(3);
+	})
 })
--- src/christmas.js
+++ src/christmas.js
@@ -1,9 +1,14 @@
 
 function drawTree (treeSize) {
-	var tree = "    *    \n";
-	tree += 	 "   * *   \n";
-	tree += 	 "  * * *  \n";
-	tree += 	 " * * * * \n";
-	tree += 	 "   |_|   \n";
+	var tree = "";
+	for (var line=0; line<treeSize; line++){
+		tree+="*";
+		var numberOfStars = line-Math.floor(line/4)*2;
+		for (var star=0;star<numberOfStars;star++){
+			tree+=" *";
+		}
+		tree += "\n";
+	}
+	tree += "|_|";
 	return tree;
 }
