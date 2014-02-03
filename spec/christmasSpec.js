ad07a40fac7023ecb426af4fe4af64848944e0c5
codingDojo1 <guillaume.billey@inist.fr>
2014-02-03 16:27:41 +0100

    Resultat du coding dojo 1

--- spec/christmasSpec.js
+++ spec/christmasSpec.js
@@ -5,4 +5,37 @@ describe("The Christmas Tree", function(){
 		var treeLines = tree.split('\n');
 		expect(treeLines.length).toBe(11);
 	});
+
+	it("Le paramètre d'entrée doit être supérieur à 3", function(){
+		var smallTree = function() {
+			drawTree(2);
+		}
+		var bigTree = function() {
+			drawTree(5);
+		}
+		expect(smallTree).toThrow();
+		expect(bigTree).not.toThrow();
+	});
+
+	it("Devrait avoir chaque ligne avec X etoiles, separées par un espace", function (){
+		var tree = drawTree(10);
+		tree.split("\n").forEach(function (line, lineIdx) {
+			var nbEtoilesATester = lineIdx + 1;
+			if (lineIdx == 10) return; // LE TRONC !!!
+			var result = line.split("*"); // LES BRANCHES !!!
+			for (var i = 1; i < result.length - 1; i++) {
+				var supposedSpace = result[i];
+				expect(supposedSpace).toBe(" ");
+			};
+			expect(result.length).toBeGreaterThan(0);
+		});
+	});
+
+
+	it("Toutes les 4 lignes, le nombre d'étoiles est réduit d'un", function(){
+		var tree = drawTree(5);
+		var ligne4=tree.split("\n")[4];
+		var comptage = ligne4.match(/\*/g).length;
+		expect(comptage).toBe(3);
+	});
 })
--- src/christmas.js
+++ src/christmas.js
@@ -1,5 +1,20 @@
 
 function drawTree (treeSize) {
+	if (treeSize <= 3) {
+		throw new Error("La taille est inférieure à 3");
+	};
 	var tree = "";
+	var touteLesQuatreLignes = 0;
+	for (var i = 0; i < treeSize; i++) {
+		if ((i % 5) == 0) touteLesQuatreLignes+=2;
+		var branche="";
+		for (var j = 0; j <= i - touteLesQuatreLignes; j++) {
+			branche+="* ";
+		}
+		tree+=branche.trim()+"\n";
+
+	}
+	tree+="||";
+	console.log(tree);
 	return tree;
 }
